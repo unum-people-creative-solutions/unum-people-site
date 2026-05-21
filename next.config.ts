48e335d309c9e5f8a05ab023a3e68b3db28c8188
jaredejr <jaredejr@gmail.com>
2026-05-21 14:56:55 -0300

    fix: adicionando domains e simplificando remotePatterns para compatibilidade com microlink

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['api.microlink.io'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'api.microlink.io',
-        pathname: '/**',
       },
     ],
   },
