3bb8b7944256257f4f6afcf5df7527e80afd75bb
jaredejr <jaredejr@gmail.com>
2026-05-21 15:03:28 -0300

    fix: adicionando domínios próprios à whitelist do next/image por segurança

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['api.microlink.io'],
+    domains: [
+      'api.microlink.io',
+      'unumpeople.com.br',
+      'www.unumpeople.com.br'
+    ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'api.microlink.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unumpeople.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.unumpeople.com.br',
       },
     ],
   },
